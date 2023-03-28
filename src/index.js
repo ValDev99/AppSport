8bfbd2a2d2a5d7049e1b2eb7ce71cf045d478cd9
Valentin Mignon <valentin54112@gmail.com>
2023-03-28 21:44:13 +0200

    dymaJs15end

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const name = "jean";
+
+const age = 18;
+
+const hobbies = null;
+
+let action;
+
+let sym = Symbol();
+
+let actif = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof hobbies);
+console.log(typeof action);
+console.log(typeof sym);
+console.log(typeof actif);
+
+const city = {
+    name:"paris",
+    state: "france"
+};
+
+const greetings = function() {
+    console.log("hello");
+};
+
+const today = new Date();
+
+const arr = [1,2,3];
+
+console.log(typeof city);
+console.log(typeof greetings);
+console.log(typeof today);
+console.log(typeof arr);
